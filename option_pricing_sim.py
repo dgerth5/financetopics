29713458aec33870c47047f21f24492791515380
dgerth5 <33240415+dgerth5@users.noreply.github.com>
2023-09-13 18:46:44 -0500

    Update option_pricing_sim.py

--- option_pricing_sim.py
+++ option_pricing_sim.py
@@ -11,8 +11,6 @@ def gbm(s0, u, o, t, steps, sims):
     
     return paths
 
-P = gbm(100, 0.5, 0.3, 1, 100, 1000)
-
 def euro_option_prices(paths, r, t, K):
     c = np.zeros(len(K))
     p = np.zeros(len(K))
@@ -24,4 +22,19 @@ def euro_option_prices(paths, r, t, K):
     df = pd.DataFrame({'Strike': K, 'Call_Price': c, 'Put_Price': p})
     return df
 
-print(euro_option_prices(P, 0.01, 1, [95]))
+def asian_option_prices(paths, r, t, K):
+    c = np.zeros(len(K))
+    p = np.zeros(len(K))
+
+    for i in range(len(K)):
+        c[i] = round(np.mean(np.maximum(np.mean(paths, axis=0) - K[i], 0)) / np.exp(r * t), 2)
+        p[i] = round(np.mean(np.maximum(K[i] - np.mean(paths, axis=0), 0)) / np.exp(r * t), 2)
+
+    df = pd.DataFrame({'Strike': K, 'Call_Price': c, 'Put_Price': p})
+    return df
+
+# test 
+P = gbm(100, 0.5, 0.3, 1, 100, 1000)
+print(euro_option_prices(P, 0.01, 1, [95,96,97]))
+print(asian_option_prices(P, 0.01, 1, [95,96,97]))
+
